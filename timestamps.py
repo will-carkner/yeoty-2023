724751ca5402ec071981dfdad227457ab310ec21
Folded-Code <34174315+Folded-Code@users.noreply.github.com>
2023-02-23 10:00:32 +0000

    improved timestamps creator

--- timestamps.py
+++ timestamps.py
@@ -1,11 +1,19 @@
 import csv
 
 headers = ['time', 'howth_count', 'sutton_count']
-with open('data/thurs-m.csv', 'w') as f:
+fileName = 'data/test-m.csv'
+
+startH = 7
+endH = 9
+startM = 0
+endM = 45
+
+with open(fileName, 'w') as f:
     writer = csv.writer(f)
     # write header
     writer.writerow(headers)
     # write timestamps from 7:00 to 9:45 in 5 min increments
-    for hour in range(7, 10):
-        for minute in range(0, 60, 5):
+    # stop at 9:40
+    for hour in range(startH, endH + 1):
+        for minute in range(0, endM if hour == endH else 60, 5):
             writer.writerow([f'{hour}:{minute:02}', 0, 0])
